2cd1f83c5bc5d1f65575fc44f4d445f1ba4489d8
Aakashak04 <148637391+Aakashak04@users.noreply.github.com>
2024-07-21 17:46:38 +0530

    Create Login.js

---
+++ src/components/Login.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import '../Login.css'; 
+
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (username === 'aakash' && password === 'aakash002') {
+      onLogin(true);
+    } else {
+      alert('Incorrect username or password');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
