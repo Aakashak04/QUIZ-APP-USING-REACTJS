e26b27dd892931f41b448b23cbee254f4d907f8c
Aakashak04 <148637391+Aakashak04@users.noreply.github.com>
2024-07-21 17:48:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Quiz from './components/quiz';
+import Login from './components/Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div id="app-container">
       <header>
         <h1>Quiz App</h1>
       </header>
-      <Quiz />
+      {isLoggedIn ? (
+        <Quiz />
+      ) : (
+        <Login onLogin={setIsLoggedIn} />
+      )}
     </div>
   );
 }
